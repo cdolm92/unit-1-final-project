810272f4f847dc3598aba9a6f181129dfaf4931a
cdolm92 <cdolm92@gmail.com>
2015-08-22 17:59:14 -0400

    first commit

---
+++ Time/Time/CDTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CDTableViewController.h
+//  Time
+//
+//  Created by Christella on 8/22/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDTableViewController : UITableViewController
+
+@end
---
+++ Time/Time/SWViewController.h
@@ -0,0 +1,13 @@
+//
+//  SWViewController.h
+//  Time
+//
+//  Created by Christella on 8/22/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWViewController : UIViewController
+
+@end
